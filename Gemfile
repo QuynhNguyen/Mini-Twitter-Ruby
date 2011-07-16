acd51abc2a50bdafe0022e89046517bbe2f55f95
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-07-16 16:46:36 -0700

    added site layout, refractor, more testing with rspect

--- Gemfile
+++ Gemfile
@@ -5,10 +5,12 @@ gem 'rake', '0.8.7'
 
 group :development do
 	gem 'rspec-rails', '2.6'
+	gem 'webrat'
 end
 
 group :test do
 	gem 'rspec', '2.6'
+	gem 'webrat'
 end
 
 # Bundle edge Rails instead:
@@ -35,6 +37,6 @@ gem 'sqlite3'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
+group :development, :test do
+	gem 'webrat'
+end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,17 +2,46 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+    	get 'home'
+    	response.should have_selector("title", :content => "Ruby on Rails Tutorial Smaple App | Home")
+    end
+    
+    it "should have non-blank body" do
+    	get 'home'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
+  
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+    	get 'contact'
+    	response.should have_selector("title", :content => "Ruby on Rails Tutorial Smaple App | Contact")
+    end
+  end 
+  
+  describe "GET 'about'" do
+  	it "should be successful" do
+  		get 'about'
+  		response.should be_success
+  	end
+  	it "should have the right title" do
+    	get 'about'
+    	response.should have_selector("title", :content => "Ruby on Rails Tutorial Smaple App | About")
     end
   end
 
